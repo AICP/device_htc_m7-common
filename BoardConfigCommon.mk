be60307e9d427a963cfdf4a1e935273d7dcc9c2b
LorDClockaN <davor@losinj.com>
2013-09-24 20:44:31 +0200

    Change kernel defconfig for aicp
    Change-Id: If2323dad27436ce0bf4570acab132142c856a474

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,7 @@ BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
-TARGET_KERNEL_CONFIG := cyanogenmod_m7_defconfig
+TARGET_KERNEL_CONFIG := aicp_m7_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/m7
 
 # Custom flags
