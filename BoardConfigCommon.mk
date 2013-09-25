c5c48813ea7e65efd5791ae62723a0e076c22ba4
Ethan Chen <intervigil@gmail.com>
2013-09-25 15:00:42 -0700

    m7-common: clean up initscripts
    * m7spr has it's own set, while m7vzw shares the GSM set
    * Fixes compilation for non-m7vzw variants
    
    Change-Id: I53f877d132d8ce2c15144ff12f82232fae883c4f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,10 +91,10 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 27917287424
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Custom Recovery
-ifneq ($(filter m7att m7tmo m7ul m7vzw,$(TARGET_DEVICE)),)
-TARGET_RECOVERY_FSTAB := device/htc/m7-common/rootdir/etc/fstab.qcom.gsm
+ifeq ($(TARGET_DEVICE),m7spr)
+TARGET_RECOVERY_FSTAB := device/htc/m7-common/rootdir/etc/fstab.qcom.spr
 else
-TARGET_RECOVERY_FSTAB := device/htc/m7-common/rootdir/etc/fstab.qcom.cdma
+TARGET_RECOVERY_FSTAB := device/htc/m7-common/rootdir/etc/fstab.qcom.gsm
 endif
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_HAS_NO_SELECT_BUTTON := true
