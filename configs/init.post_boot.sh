79650c14ae7520e8a2a7b701247272dcf3295101
doc <doc.divxm@gmail.com>
2014-09-14 08:42:22 +0300

    Revert "init.postboot.sh -> change from ondemand to smartmax governor"
    This reverts commit d506619dfda94b85b142a5dc91e738ddeefba997.
    
    Change-Id: I858493a3d5eedfcb824aba568b49e29e2983378a

--- configs/init.post_boot.sh
+++ configs/init.post_boot.sh
@@ -50,24 +50,24 @@ case "$target" in
         echo 1 > /sys/devices/system/cpu/cpu1/online
         echo 1 > /sys/devices/system/cpu/cpu2/online
         echo 1 > /sys/devices/system/cpu/cpu3/online
-        echo "smartmax" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        echo "smartmax" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-        echo "smartmax" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-        echo "smartmax" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-        # echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
-        # echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
-        # echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
-        # echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-        # echo 10 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
-        # echo 60 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_multi_core
-        # echo 3 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential_multi_core
-        # echo 918000 > /sys/devices/system/cpu/cpufreq/ondemand/optimal_freq
-        # echo 1026000 > /sys/devices/system/cpu/cpufreq/ondemand/sync_freq
-        # echo 1134000 > /sys/devices/system/cpu/cpufreq/ondemand/input_boost
-        # echo 80 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
-        # chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
-        # chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-        # chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+        echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+        echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+        echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+        echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+        echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
+        echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+        echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+        echo 10 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
+        echo 60 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_multi_core
+        echo 3 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential_multi_core
+        echo 918000 > /sys/devices/system/cpu/cpufreq/ondemand/optimal_freq
+        echo 1026000 > /sys/devices/system/cpu/cpufreq/ondemand/sync_freq
+        echo 1134000 > /sys/devices/system/cpu/cpufreq/ondemand/input_boost
+        echo 80 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
+        chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+        chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+        chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
         echo 20 > /sys/module/cpu_boost/parameters/boost_ms
         echo 1566000 > /sys/module/cpu_boost/parameters/sync_threshold
         echo 1134000 > /sys/module/cpu_boost/parameters/input_boost_freq
