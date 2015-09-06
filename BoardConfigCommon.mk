75293499727f163a40b27e51db4d1b6c449e3f8a
Michael Bestas <mikeioannina@gmail.com>
2015-09-06 12:30:17 +0300

    m7-common: optimize ART
    --Turn off ART watchdog
    
    There is a time limit for optimization of apk.
    Some apps like play services takes a lot of time,
    so it doesn't gets optimized properly due to time limit
    and tries to optimize on each both.It also happens when it's updated.
    By disabling watchdog we let the apk to get optimized completely.
    
    Change-Id: Ifcb12341c6f0cdeae6e6983da7f2a7613a6ea485

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,6 +39,11 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
 TARGET_KERNEL_CONFIG := m7_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  WITH_DEXPREOPT := true
+endif
+
 # Audio
 BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
 BOARD_USES_SEPERATED_AUDIO_INPUT := true  # use distinct voice recognition use case
