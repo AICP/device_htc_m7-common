0aadf1d0e07f0cb9da6051a222fd3ab8be89c957
Rashed Abdel-Tawab <rashed@linux.com>
2018-10-11 14:46:37 +0300

    m7-common: Ignore Selinux Neverallows
    Change-Id: I585ae6bac9d0ed303422f8dfa85ecf83932bb2cd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -92,6 +92,11 @@ BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
 
 BOARD_SEPOLICY_DIRS += device/htc/m7-common/sepolicy
 
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Wifi
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
