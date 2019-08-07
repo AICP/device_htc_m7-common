7fa222dff8fb89fb33ab98f52de1fc57406b2242
Bruno Martins <bgcngm@gmail.com>
2019-08-07 01:15:33 +0100

    m7-common: camera: Do the shuffle with CameraParameters
    Up until N it was possible for devices to hook extra classes and
    symbols into libcamera_client. Starting with O, several makefiles
    were converted to blueprints, camera headers changed place and
    overriding headers became almost impossible.
    
    Due to the nature of HTC's MSM8974 camera HAL, which expects
    CameraParameters to inherit from CameraParameters_EXT, injecting
    the custom CameraParameters code like in the past now resulted
    in mediaserver fatal crashes due to mismatched headers.
    
    Ship a kind of shim as a workaround to those issues and satisfy
    the camera HAL.
    
    Change-Id: I85055e485653e8e93424428fd0dc429f8c76f94f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,6 +1,6 @@
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2013 The Android Open Source Project
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -61,7 +61,6 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
     /system/bin/qseecomd=21
 
 USE_DEVICE_SPECIFIC_CAMERA := true
-TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY := camera_parameters_htc_msm8960
 
 # Charger
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
