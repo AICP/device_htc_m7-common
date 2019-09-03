19f5c2dbdf27ea9a2044c32871f205013f2687d3
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 03:20:57 +0430

    msm8960-common: Force disable bluetooth audio hal 2.0
    Change-Id: I217becb041395865a1c6f997b0cbc0c994d04e78

--- system_prop.mk
+++ system_prop.mk
@@ -14,7 +14,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bt.bdaddr_path=/sys/module/htc_bdaddress/parameters/bdaddress
+    ro.bt.bdaddr_path=/sys/module/htc_bdaddress/parameters/bdaddress \
+    persist.bluetooth.bluetooth_audio_hal.disabled=true
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
