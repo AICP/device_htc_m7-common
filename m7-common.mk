441e1cef7b6c56ef129672910bcc1d2c3c1c4f6b
Julian Veit <claymore1298@gmail.com>
2020-04-18 16:16:51 +0200

    m7-common: remove com.android.nfc_extras.xml from PRODUCT_COPY_FILES

--- m7-common.mk
+++ m7-common.mk
@@ -105,7 +105,6 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
