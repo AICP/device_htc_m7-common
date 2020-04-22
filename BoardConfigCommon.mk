5cc0d94fdeeafb75392150ecd5ebb0918e92fb31
Julian Veit <claymore1298@gmail.com>
2020-04-22 09:00:28 +0200

    m7-common: Set SDK API level for qseecomd
    Change-Id: I9ea3e15f322be9e161c9289b31144f402632ffe2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,7 +49,8 @@ TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_DISPLAY_INSECURE_MM_HEAP := true
 TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
     /system/bin/mediaserver=21 \
-    /system/vendor/bin/mm-qcamera-daemon=21
+    /system/vendor/bin/mm-qcamera-daemon=21 \
+    /system/bin/qseecomd=21
 USE_DEVICE_SPECIFIC_CAMERA := true
 
 # Charger
