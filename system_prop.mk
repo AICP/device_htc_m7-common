5cd48475201ae851ab152ba8f8eb920474632a57
Julian Veit <claymore1298@gmail.com>
2020-04-22 22:57:35 +0200

    m7-common: set density to 420
    Change-Id: Iceeedaa0ae55e9539a1a896300e309b6dc6929a8

--- system_prop.mk
+++ system_prop.mk
@@ -32,7 +32,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608 \
-    ro.sf.lcd_density=480
+    ro.sf.lcd_density=420
 
 # Input
 PRODUCT_PROPERTY_OVERRIDES += \
