41f349c996b259e944da2115a9441e4dc855c5a6
Julian Veit <claymore1298@gmail.com>
2020-04-23 22:26:55 +0200

    m7-common: change audio props to match hal
    Change-Id: I1c09c6c0ad1f58e168b25d626cc889cbb28e011b

--- system_prop.mk
+++ system_prop.mk
@@ -4,15 +4,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    audio.offload.disable=1 \
+    audio.offload.disable=true \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0 \
-    persist.audio.fluence.speaker=true \
-    persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.voicerec=false \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
     ro.qc.sdk.audio.fluencetype=fluence \
-    use.dedicated.device.for.voip=true \
-    use.voice.path.for.pcm.voip=true
+    vendor.dedicated.device.for.voip=true \
+    vendor.voice.path.for.pcm.voip=true
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
