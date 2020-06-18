1f8dd9c9902580fe54feb8d01b607b01f824db51
Julian Veit <claymore1298@gmail.com>
2020-06-18 13:32:44 +0200

    m7-common: DeviceParts: move to kotlin
    Change-Id: I30bb3c0b9a1d4110c3857211773325823fc3f34b

---
+++ DeviceParts/src/com/aicp/device/DeviceSettingsActivity.kt
@@ -0,0 +1,65 @@
+/*
+* Copyright (C) 2017 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.aicp.device
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+//import android.app.DialogFragment
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+//import android.app.Fragment
+import androidx.fragment.app.Fragment
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.preference.PreferenceFragment
+import androidx.preference.PreferenceManager
+
+class DeviceSettingsActivity : FragmentActivity() {
+    private var mDeviceSettingsFragment: DeviceSettings? = null
+    override protected fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getActionBar().setDisplayHomeAsUpEnabled(true)
+
+        val fragment: Fragment? = getSupportFragmentManager().findFragmentById(android.R.id.content)
+        if (fragment == null) {
+            mDeviceSettingsFragment = DeviceSettings()
+            getFragmentManager().beginTransaction()
+                    .add(android.R.id.content, mDeviceSettingsFragment)
+                    .commit()
+        } else {
+            var mDeviceSettingsFragment = fragment as DeviceSettings?
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            else -> {
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
