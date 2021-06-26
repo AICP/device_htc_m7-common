e680e9908984a141eb61255c68291a2867f755fc
Julian Veit <claymore1298@gmail.com>
2021-06-26 13:35:13 +0200

    m7-common: rootdir: add loggy.sh
    Change-Id: I16cc3f6a8c8849afe9cfcc31ee2eb177941122e3

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -48,3 +48,12 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := loggy.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := etc/loggy.sh
+LOCAL_VENDOR_MODULE    := true
+include $(BUILD_PREBUILT)
+
