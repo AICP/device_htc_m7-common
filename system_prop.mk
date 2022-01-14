c046bfa6570be366a5e504657c1ee7d75feb8754
Julian Veit <claymore1298@gmail.com>
2022-01-14 17:04:56 +0100

    m7-common: Remove duplicated properties
    Change-Id: I8f5c94841423e5e8f8cb659a88f398b1df25f8ee

--- system_prop.mk
+++ system_prop.mk
@@ -25,8 +25,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Dalvik heap
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
